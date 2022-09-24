2ede5aa0723a50e7d5f23909e84c075b7cd9ea42
rnewt6926 <rnewt6926@gmail.com>
2022-09-23 20:48:39 -0500

    If Exist

--- pull_pii.sh
+++ pull_pii.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [[ -f "/var/www/html/dvwa/hackable/uploads/PII.zip" ]]
+then
+    rm "/var/www/html/dvwa/hackable/uploads/PII.zip"
+    echo "PII.zip Was Deleted"
+fi
+
 smbclient '//192.168.122.7/Private_Files' -c 'lcd /var/www/html/dvwa/hackable/uploads; get PII.zip' -W HACKYSOC -U wier%frantic-Folly3 
 chmod 775 /var/www/html/dvwa/hackable/uploads/PII.zip
+
 echo 'Finished'
