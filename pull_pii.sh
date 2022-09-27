b1800978b54257c0b190b1e56f8ebf754e59b7d0
rnewt6926 <rnewt6926@gmail.com>
2022-09-27 18:56:35 -0500

    Updated PII lift

--- pull_pii.sh
+++ pull_pii.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if [[ -f "/var/www/html/dvwa/hackable/uploads/PII.zip" ]]
-then
-    rm -f "/var/www/html/dvwa/hackable/uploads/PII.zip"
-    echo "PII.zip Was Deleted"
-fi
 
-smbclient '//192.168.122.7/Private_Files' -c 'lcd /var/www/html/dvwa/hackable/uploads; get PII.zip' -W HACKYSOC -U wier%frantic-Folly3 
-chmod 775 '/var/www/html/dvwa/hackable/uploads/PII.zip'
+# 
+# 
+# if [[ -f "/var/www/html/dvwa/hackable/uploads/PII.zip" ]]
+# then
+#     rm -f "/var/www/html/dvwa/hackable/uploads/PII.zip"
+#     echo "PII.zip Was Deleted"
+# fi
+#
+# 
+
+
+smbclient '//192.168.122.7/Private_Files' -c 'lcd /var/tmp; get PII.zip.ibkngP' -W HACKYSOC -U wier%frantic-Folly3 
+chmod 775 '/var/tmp/PII.zipibkngP'
+
+smbclient '//192.168.122.7/Private_Files' -c 'lcd /var/tmp; get rogeradamsCV.doc.49t4' -W HACKYSOC -U wier%frantic-Folly3
+chmod 775 '/var/tmp/rogeradamsCV.doc.49t4'
 
 echo 'Finished'
