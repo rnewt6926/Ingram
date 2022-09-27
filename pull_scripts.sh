73a4e7d628793f3fc8b0fc9e0e0d3b357a10dc43
rnewt6926 <rnewt6926@gmail.com>
2022-09-27 13:05:50 -0500

    Update pull_scripts

---
+++ pull_scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -f "/var/tmp/pull_scripts.sh" ]]
+then
+    rm -f "/var/tmp/pull_scripts.sh"
+    echo "pull_scripts Was Deleted"
+fi
+
+cd /var/tmp
+wget https://github.com/rnewt6926/Ingram/raw/main/pull_pii.sh
+
+echo "Done"
